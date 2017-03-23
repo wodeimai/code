2ff700be58a327afc7919e34fb038c8e1fe096b6
BiteLips <1014944047@qq.com>
2017-03-23 21:46:26 +0800

    update get_detail_content

--- Application/Go/Controller/CollectController.class.php
+++ Application/Go/Controller/CollectController.class.php
@@ -35,7 +35,7 @@ class CollectController extends Controller
 
     public function detail()
     {
-        $id = '3801159';
+        $id = '3807486';
         $this->get_detail_content($id);
     }
 
@@ -43,15 +43,15 @@ class CollectController extends Controller
     {
         Vendor('phpQuery.phpQuery.phpQuery');
         $url = "http://guangdiu.com/detail.php?id=" . $id;
-        \phpQuery::newDocumentFile($url);        
+        \phpQuery::newDocumentFile($url);
 
         // div class=detaildisplay
-        $html = pq('.detaildisplay'); 
+        $html = pq('.detaildisplay');
 
         // 以下为div class=detaildisplay所有的子元素
         // link地址
         $dlink = pq($html)->find('.dtitlegotobuy')->attr('href');
-        
+
         // class=dtitle 标题
         $dtitle = pq($html)->find('.dtitle a')->html();
 
@@ -64,50 +64,20 @@ class CollectController extends Controller
         $dabstract = pq($html)->find('#dabstract');
 
         foreach (pq($dabstract)->find('p') as $key => $value) {
-            $p[$key] = pq($value)->html();
+            $p['p_' . ($key + 1)] = pq($value)->html();
         }
 
         foreach (pq($dabstract)->find('img') as $key => $value) {
-            $img[$key] = pq($value)->attr('src');
+            $img['pic_' . ($key + 1)] = pq($value)->attr('src');
         }
 
-        // $p1 = pq($dabstract)->find('p')->eq(0)->html();
-        // $p2 = pq($dabstract)->find('p')->eq(1)->html();
+        if (empty($p)) {
+            $p['p1'] = pq($dabstract)->html();
+        }
 
-        var_dump($p);
-        var_dump($img);
+        dump($p);
+        echo "<br/>";
+        dump($img);
         die;
-
-
-
-
-        // 以下为div class=dabstract下所有的子元素
-        // 最先显示图片且为一张(形式一)
-        $dimage = pq($dabstract)->find('.dimage img')->attr('src');
-        if(!empty($dimage)){
-            $dimage_id = save_jpg($dimage, $id);
-        }
-        // 最先显示图片且为一张(形式二)
-        $dt = pq($dabstract)->find('dt img')->attr('src');
-        if(!empty($dt)){
-            $dt_id = save_jpg($dt, $id);
-        }
-        // 最先显示图片且为一张(形式三)
-        $editor_mod = pq($dabstract)->find('editor-mod img')->attr('src');
-        if(!empty($editor_mod)){
-            $editor_mod_id = save_jpg($editor_mod, $id);
-        }
-
-        // 最先显示图片且为两张
-        $simgaheadleft = pq($dabstract)->find('.simgaheadleft img')->attr('src');
-        $simgaheadleft_id = save_jpg($simgaheadleft, $id);
-        $simgaheadright = pq($dabstract)->find('.simgaheadright img')->attr('src');
-        $simgaheadright_id = save_jpg($simgaheadright, $id);
-        $simgahead = $simgaheadleft_id . ',' . $simgaheadright_id;
-
-        // div 文本内容(形式一)
-        $hui_content_text = pq($dabstract)->find('.hui_content_text')->html();
-        // div 文本内容(形式二)
-        $huimabstract = pq($dabstract)->find('.dabstract')->html();
     }
 }
