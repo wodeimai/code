4cf5eb63f52cc459c9a1676311af5e3dff1e3230
szgekko <44727340@qq.com>
2017-04-28 16:47:27 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -11,8 +11,8 @@ class IndexController extends HomeController {
 		parent::_initialize();
 
 		$this->goods_model = D('wdm_goods');
+		$this->malls_model = D('wdm_mall');
 
-		$this->set_array = $array;
 	}
 
 	//系统首页
@@ -39,7 +39,6 @@ class IndexController extends HomeController {
 		foreach ($mall_list as $key => $value) {
 			$new_mall_list[$value['id']] = $value['mall_name'];
 		}
-
 		foreach ($list_data['list_data'] as &$value) {
 			$value['source_time'] = time_tran($value['source_time']);
 			$value['target'] = $new_mall_list[$value['target']];
@@ -48,6 +47,8 @@ class IndexController extends HomeController {
 		$this->assign($list_data);
 
 		//获取商场数据
+		$mall_list = $this->malls_model->where(array('type' => '1'))->order('sort desc')->limit('10')->select();
+		$this->assign('mall_list', $mall_list);
 
 		//获取今日商品数和预计总数
 		$today_time = strtotime(date('Y-m-d', time()));
@@ -59,6 +60,10 @@ class IndexController extends HomeController {
 		$this->assign('total', $total);
 
 		//10日总数
+		$ten_days_number = strtotime(date('Y-m-d', strtotime('-10 days')));
+		$where_total['source_time'] = array('EGT', $ten_days_number);
+		$total_count = $this->goods_model->where($where_total)->count();
+		$this->assign('total_count', $total_count);
 
 		//当前类别
 		$type = I('type') == 'haitao' ? '海淘' : '国内';
@@ -68,11 +73,9 @@ class IndexController extends HomeController {
 	}
 
 	public function get_news() {
-		// $model = M();
-		// $maxid = I('maxid');
-		// $map['id'] = array('gt', $maxid);
-		// $news_cnt = $model->where($map)->count();
-		$news_cnt = 1;
+		$maxid = I('maxid');
+		$map['id'] = array('gt', $maxid);
+		$news_cnt = $this->goods_model->where($map)->count();
 		$this->ajaxReturn(array('cnt' => $news_cnt));
 	}
 }
