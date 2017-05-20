2fa17f6ae9d24806c678995ef1094537835c3021
szgekko <44727340@qq.com>
2017-05-20 23:45:08 +0800

    update

--- Application/Runtime/Data/_fields/wodeimai.dei_wdm_mall.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"id";i:1;s:4:"type";i:2;s:9:"mall_name";i:3;s:4:"sort";s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:16:"int(10) unsigned";s:4:"type";s:10:"tinyint(1)";s:9:"mall_name";s:11:"varchar(50)";s:4:"sort";s:16:"int(10) unsigned";}}
